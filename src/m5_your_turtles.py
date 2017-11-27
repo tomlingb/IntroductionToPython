0aa9e1727cbd583c6315e13422f61f8d82bff47a
tomlingb <tomlingb@rose-hulman.edu>
2017-11-27 14:46:52 -0500

    Changed speed

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Geoffrey Tomlinson.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,8 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow
+
+Bob = rg.SimpleTurtle
